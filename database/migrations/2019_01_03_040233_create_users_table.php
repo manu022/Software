c3feb1da7424367bcbea380ccc0c73653e7888bb
root <manuel.grandond@utem.cl>
2019-01-03 01:34:39 -0300

    base de datos

--- database/migrations/2019_01_03_040233_create_users_table.php
+++ database/migrations/2019_01_03_040233_create_users_table.php
@@ -15,11 +15,10 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
+            $table->string('name',100);
+            $table->integer('rut')->unique();
+            $table->string('email',100)->unique();
             $table->string('password');
-            $table->rememberToken();
             $table->timestamps();
         });
     }
