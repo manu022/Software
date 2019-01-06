3aae802bdfa0baa8206d44fbcee36fdf98c6ee2c
root <manuel.grandond@utem.cl>
2019-01-06 02:05:18 -0300

    seed

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -1,12 +1,13 @@
 <?php
-
+use Illuminate\Database\Seeder;
+use Illuminate\Foundation\Auth\User;
 /**
 * Agregamos un usuario nuevo a la base de datos.
 */
 class UserTableSeeder extends Seeder {
     public function run(){
         User::create(array(
-            'rut'  => 19290556,
+            'rut'  => '19290556',
             'email'     => 'admin@admin.com',
             'name'=> 'Administrator',
             'password' => Hash::make('admin') // Hash::make() nos va generar una cadena con nuestra contraseña encriptada
