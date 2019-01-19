dd9b25a2f430be291223914e49e71c0de1bd009f
CamiloMonares <camilo.monaresg@utem.cl>
2019-01-19 05:24:25 -0300

    Ahora el pdf generado captura lo escrito en el formulario de la vista -pruebados-, la idea es pasar esoa la vista curriculum pero no tengo acceso a ella porque me manda al login asi que no puedo llenar los campos para hacer pruebas y ver que los pasa bien a pdf

--- public/crear-pdf.php
+++ public/crear-pdf.php
@@ -1,25 +1,29 @@
 <?php
-  ob_start();
-  ?>
-<page backtop="10mm" backbottom="10mm" backleft="20mm" backright="20mm">
- <h1>Pdf creado de prueba</h1>       <br>
-  <b>Esto será el curriculum, ir a la pagina principal</b><br>
-  <a href='home'>Click</a>.<br>
-</page>
+
+print ("Nombre: ");
+echo $_POST["Nombre"];
+echo "<br>";
+print ("Apellidos: ");
+echo $_POST["Apellidos"];
+echo "<br>";
+echo $contraseña= $_POST["Contraseña"];
+echo "<br>";
+
+ ?>
+
 
 <?php
   $content = ob_get_clean();
   require_once(dirname(__FILE__).'/../vendor/autoload.php');
   use Spipu\Html2Pdf\Html2Pdf;
-  use Spipu\Html2Pdf\Exception\Html2PdfException;
-  use Spipu\Html2Pdf\Exception\ExceptionFormatter;
 
   try
   {
 
+      ob_start();
       $html2pdf = new HTML2PDF('P', 'A4', 'es', true, 'UTF-8', 3);
       $html2pdf->pdf->SetDisplayMode('fullpage');
-      $html2pdf->writeHTML($content, isset($_GET['vuehtml']));
+      $html2pdf->writeHTML($content, isset($_POST['prueba']));
       ob_end_clean();
       $html2pdf->Output('PDF-CF.pdf');
   }
--- resources/views/pruebados.blade.php
+++ resources/views/pruebados.blade.php
@@ -2,7 +2,7 @@
 <html lang="en">
     <head>
         <meta charset="UTF-8">
-        <title>Generar tu curriculum</title>
+        <title>Tu curriculum</title>
         <link rel="stylesheet" href="https://code.getmdl.io/1.2.1/material.indigo-pink.min.css" />
         <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
         <script defer src="https://code.getmdl.io/1.2.1/material.min.js"></script>
@@ -10,7 +10,7 @@
     <body>
         <div class="demo-card-wide mdl-card mdl-shadow--2dp">
           <div class="mdl-card__title">
-            <h2 class="mdl-card__title-text">Crear un PDF con tu curriculum</h2>
+            <h2 class="mdl-card__title-text">Crear un PDF</h2>
           </div>
           <div class="mdl-card__supporting-text">
             Creador de curriculums
@@ -26,5 +26,19 @@
             </button>
           </div>
         </div>
-    </body>
+          <form method = "post" action="crear-pdf.php">
+              Nombre: <input type="text" name="Nombre">
+              <br>
+              Apellidos: <input type="text" name="Apellidos">
+              Contraseña <input type="password" name="Contraseña">
+              <input type="submit"  value="Enviar">
+            </form>
 </html>
+
+
+
+
+<?php
+
+
+ ?>
